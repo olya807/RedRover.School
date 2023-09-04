9d0895288c49bffd434025f2a1aca69c831fedea
Volha <morozolga92@gmail.com>
2023-09-04 15:49:37 +0300

    add class Employee1, class Manager, class BaseEmployee

---
+++ src/Employee1.java
@@ -0,0 +1,10 @@
+public class Employee1 extends BaseEmployee {
+
+    public Employee1(String name, int age, char gender, int daySalary) {
+        super(name, age, gender, daySalary);
+    }
+
+    public int detSalary(int days) {
+        return getDaySalary() * days;
+    }
+}
